68dd3fb82d6f3753b667f4af1a1c5046b660988f
aminos <amri.amine@protonmail.com>
2019-05-12 12:32:33 +0100

    illustrating types

---
+++ ch1/types.lua
@@ -0,0 +1,30 @@
+print("type(nil)")
+print(type(nil))
+
+print()
+print("type(true)")
+print(type(true))
+
+print()
+print("type(10.4 * 3)")
+print(type(10.4 * 3))
+
+print()
+print("type(\"Hellow world\")")
+print(type("Hello world"))
+
+print()
+print("type(io.stdin)")
+print(type(io.stdin))
+
+print()
+print("type(type)")
+print(type(type))
+
+print()
+print("type({})")
+print(type({}))
+
+print()
+print("type(type(1)")
+print(type(type(1)))
