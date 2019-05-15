bf9de73160247298bb99a39a53a70c1365e13a4f
aminos <amri.amine@protonmail.com>
2019-05-15 21:22:36 +0100

    type() vs math.type()

---
+++ ch3/math-type.lua
@@ -0,0 +1,11 @@
+#!/bin/env lua
+
+print(4)
+print(0.4)
+
+print("using type(4) " .. type(4))
+print("using math.type(4) " .. math.type(4))
+
+print(1 == 1.0)
+print(-3 == -3.0)
+print(0.2e3 == 200)
